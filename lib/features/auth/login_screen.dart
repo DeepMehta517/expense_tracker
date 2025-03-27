314a2b457cd2263b1e9a2169093e06a9233a3ac4
DeepMehta517 <deepmehta8855@gmail.com>
2025-03-27 19:00:09 +0530

    removed prefilled login credentials

--- lib/features/auth/login_screen.dart
+++ lib/features/auth/login_screen.dart
@@ -12,8 +12,8 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
-  final TextEditingController emailController = TextEditingController(text: "deepmehta185@gmail.com");
-  final TextEditingController passwordController = TextEditingController(text: "Dm@123456789");
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
   final _formKey = GlobalKey<FormState>();
 
   @override
